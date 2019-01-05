846da3f0c12e958d5816d3af7586deccdc47672a
Rchal <yangruihan1997@gmail.com>
2019-01-05 20:09:21 +0800

    add precision control

--- logger.py
+++ logger.py
@@ -9,15 +9,9 @@ class Logger():
     def __init__(self, experiment_id, env_name, seed):
 
         format = "%(asctime)s %(threadName)s %(levelname)s: %(message)s"
-        log_formatter = logging.Formatter(format)
         logging.basicConfig(level=logging.INFO, format=format)
-        root_logger = logging.getLogger()
+        np.set_printoptions(precision=4)
 
-        # # add file handle
-        # filename = os.path.join(dir, datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "pid-%d" %(os.getpid()) + ".log")
-        # file_handler = logging.FileHandler(filename)
-        # file_handler.setFormatter(log_formatter)
-        # root_logger.addHandler(file_handler)
         work_dir = os.path.join("log", env_name, str(seed),  experiment_id )
         if os.path.exists( work_dir ):
             shutil.rmtree(work_dir)
@@ -43,7 +37,7 @@ class Logger():
         logging.info("EPOCH:{}".format(epoch_num))
         logging.info("Time Consumed:{}s".format(total_time))
 
-        tabulate_list = []
+        tabulate_list = [["Name", "Value"]]
         for info in infos:
             self.tf_writer.add_scalar( info, infos[info], total_frames )
             tabulate_list.append([ info, infos[info] ])
@@ -53,7 +47,7 @@ class Logger():
         for info in self.stored_infos:
             temp_list = [info]
 
-            temp_list.append( np.mean(self.stored_infos[info]) )
+            temp_list.append( "{}".format(np.mean(self.stored_infos[info])) )
             temp_list.append( np.std(self.stored_infos[info]) )
             temp_list.append( np.max(self.stored_infos[info]) )
             temp_list.append( np.min(self.stored_infos[info]) )
