109fe258b3df6c67c1e1b63505a5257bb6d6960d
RchalYang <ruihanyang97@gmail.com>
2020-04-08 23:32:05 -0700

    refactor network module

--- torchrl/networks/base.py
+++ torchrl/networks/base.py
@@ -4,10 +4,15 @@ import torch.nn.functional as F
 import numpy as np
 import torchrl.networks.init as init
 
+
 class MLPBase(nn.Module):
-    def __init__(self, input_shape, hidden_shapes, activation_func=F.relu, init_func = init.basic_init, last_activation_func = None ):
+    def __init__(
+            self, input_shape, hidden_shapes,
+            activation_func=nn.ReLU,
+            init_func=init.basic_init,
+            last_activation_func=None):
         super().__init__()
-        
+
         self.activation_func = activation_func
         self.fcs = []
         if last_activation_func is not None:
@@ -17,41 +22,42 @@ class MLPBase(nn.Module):
         input_shape = np.prod(input_shape)
 
         self.output_shape = input_shape
-        for i, next_shape in enumerate( hidden_shapes ):
+        for next_shape in hidden_shapes:
             fc = nn.Linear(input_shape, next_shape)
             init_func(fc)
             self.fcs.append(fc)
-            # set attr for pytorch to track parameters( device )
-            self.__setattr__("fc{}".format(i), fc)
-
+            self.fcs.append(activation_func())
             input_shape = next_shape
             self.output_shape = next_shape
 
-    def forward(self, x):
+        self.fcs.append(self.last_activation_func())
+        self.seq_fcs = nn.Sequential(*self.fcs)
 
-        out = x
-        for fc in self.fcs[:-1]:
-            out = fc(out)
-            out = self.activation_func(out)
-        out = self.fcs[-1](out)
-        out = self.last_activation_func(out)
-        return out
+    def forward(self, x):
+        return self.seq_fcs(x)
+
 
 def calc_next_shape(input_shape, conv_info):
     """
     take input shape per-layer conv-info as input
     """
     out_channels, kernel_size, stride, padding = conv_info
-    c, h, w = input_shape
+    _, h, w = input_shape
     # for padding, dilation, kernel_size, stride in conv_info:
-    h = int((h + 2*padding[0] - ( kernel_size[0] - 1 ) - 1 ) / stride[0] + 1)
-    w = int((w + 2*padding[1] - ( kernel_size[1] - 1 ) - 1 ) / stride[1] + 1)
-    return (out_channels, h, w )
+    h = int((h + 2*padding[0] - (kernel_size[0] - 1) - 1) / stride[0] + 1)
+    w = int((w + 2*padding[1] - (kernel_size[1] - 1) - 1) / stride[1] + 1)
+    return (out_channels, h, w)
+
 
 class CNNBase(nn.Module):
-    def __init__(self, input_shape, hidden_shapes, activation_func=F.relu, init_func = init.basic_init, last_activation_func = None ):
+    def __init__(
+            self, input_shape,
+            hidden_shapes,
+            activation_func=F.relu,
+            init_func=init.basic_init,
+            last_activation_func=None):
         super().__init__()
-        
+
         current_shape = input_shape
         in_channels = input_shape[0]
         self.activation_func = activation_func
@@ -60,28 +66,25 @@ class CNNBase(nn.Module):
         else:
             self.last_activation_func = activation_func
         self.convs = []
-        self.output_shape = current_shape[0] * current_shape[1] * current_shape[2]
-        for i, conv_info in enumerate( hidden_shapes ):
+        self.output_shape = current_shape[0] * \
+            current_shape[1] * current_shape[2]
+
+        for conv_info in hidden_shapes:
             out_channels, kernel_size, stride, padding = conv_info
-            conv = nn.Conv2d( in_channels, out_channels, kernel_size, stride, padding )
+            conv = nn.Conv2d(
+                in_channels, out_channels,
+                kernel_size, stride, padding)
             init_func(conv)
+
             self.convs.append(conv)
-            # set attr for pytorch to track parameters( device )
-            self.__setattr__("conv{}".format(i), conv)
-
             in_channels = out_channels
-            current_shape = calc_next_shape( current_shape, conv_info )
-            self.output_shape = current_shape[0] * current_shape[1] * current_shape[2]
-    
+            current_shape = calc_next_shape(current_shape, conv_info)
+            self.output_shape = current_shape[0] * \
+                current_shape[1] * current_shape[2]
+
+        self.seq_convs = nn.Sequential(*self.convs)
+
     def forward(self, x):
-
-        out = x
-        for conv in self.convs[:-1]:
-            out = conv(out)
-            out = self.activation_func(out)
-
-        out = self.convs[-1](out)
-        out = self.last_activation_func(out)
-
+        out = self.seq_convs(x)
         batch_size = out.size()[0]
         return out.view(batch_size, -1)
--- torchrl/networks/nets.py
+++ torchrl/networks/nets.py
@@ -1,110 +1,102 @@
 import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 import torchrl.networks.init as init
+
 
 class ZeroNet(nn.Module):
     def forward(self, x):
         return torch.zeros(1)
 
+
 class Net(nn.Module):
     def __init__(
-            self, output_shape,
+            self,
+            output_shape,
             base_type,
             append_hidden_shapes=[],
             append_hidden_init_func=init.basic_init,
             net_last_init_func=init.uniform_init,
-            activation_func=F.relu,
+            activation_func=nn.ReLU,
             **kwargs):
-
         super().__init__()
-
-        self.base = base_type(activation_func=activation_func, **kwargs )
+        self.base = base_type(activation_func=activation_func, **kwargs)
         self.activation_func = activation_func
         append_input_shape = self.base.output_shape
         self.append_fcs = []
-        for i, next_shape in enumerate( append_hidden_shapes ):
+        for next_shape in append_hidden_shapes:
             fc = nn.Linear(append_input_shape, next_shape)
             append_hidden_init_func(fc)
             self.append_fcs.append(fc)
-            # set attr for pytorch to track parameters( device )
-            self.__setattr__("append_fc{}".format(i), fc)
+            self.append_fcs.append(self.activation_func())
             append_input_shape = next_shape
 
         self.last = nn.Linear(append_input_shape, output_shape)
         net_last_init_func(self.last)
 
+        self.append_fcs.append(self.last)
+        self.seq_append_fcs = nn.Sequential(*self.append_fcs)
+
     def forward(self, x):
         out = self.base(x)
-
-        for append_fc in self.append_fcs:
-            out = append_fc(out)
-            out = self.activation_func(out)
-
-        out = self.last(out)
+        out = self.seq_append_fcs(out)
         return out
 
 
 class FlattenNet(Net):
     def forward(self, input):
-        out = torch.cat( input, dim = 1 )
+        out = torch.cat(input, dim=-1)
         return super().forward(out)
 
 
 class BootstrappedNet(nn.Module):
     def __init__(
-            self, output_shape,
+            self,
+            output_shape,
             base_type, head_num=10,
-            append_hidden_shapes=[], append_hidden_init_func=init.basic_init,
+            append_hidden_shapes=[],
+            append_hidden_init_func=init.basic_init,
             net_last_init_func=init.uniform_init,
-            activation_func=F.relu,
+            activation_func=nn.ReLU(),
             **kwargs):
-
         super().__init__()
 
-        self.base = base_type(activation_func=activation_func, **kwargs )
+        self.base = base_type(activation_func=activation_func, **kwargs)
         self.activation_func = activation_func
 
-        self.bootstrapped_append_fc = []
-        self.bootstrapped_last = []
+        self.bootstrapped_heads = []
 
         append_input_shape = self.base.output_shape
 
         for idx in range(head_num):
             append_input_shape = self.base.output_shape
             append_fcs = []
-            for i, next_shape in enumerate(append_hidden_shapes):
+            for next_shape in append_hidden_shapes:
                 fc = nn.Linear(append_input_shape, next_shape)
                 append_hidden_init_func(fc)
                 append_fcs.append(fc)
                 # set attr for pytorch to track parameters( device )
-                self.__setattr__("head_{}_append_fc{}".format(idx,i), fc)
                 append_input_shape = next_shape
 
             last = nn.Linear(append_input_shape, output_shape)
             net_last_init_func(last)
-            self.bootstrapped_last.append(last)
-            self.__setattr__("head_{}_last".format(idx), last)
-
-            self.bootstrapped_append_fc.append(append_fcs)
+            append_fcs.append(last)
+            head = nn.Sequential(*append_fcs)
+            self.__setattr__(
+                "head{}".format(idx),
+                head)
+            self.bootstrapped_heads.append(head)
 
     def forward(self, x, head_idxs):
         output = []
         feature = self.base(x)
         for idx in head_idxs:
-            out = feature
-            for append_fc in self.bootstrapped_append_fc[idx]:
-                out = append_fc(out)
-                out = self.activation_func(out)
-            out = self.bootstrapped_last[idx](out)
-
-            output.append(out)
+            output.append(self.bootstrapped_heads[idx](feature))
         return output
+
 
 class FlattenBootstrappedNet(BootstrappedNet):
     def forward(self, input, head_idxs):
-        out = torch.cat(input, dim=1)
+        out = torch.cat(input, dim=-1)
         return super().forward(out, head_idxs)
