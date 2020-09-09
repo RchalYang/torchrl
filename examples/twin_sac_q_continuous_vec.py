fix<env, algo> modify observation normalization choice

examples/twin_sac_q_continuous_vec.py
@@ -4,6 +4,8 @@ import time
 import sys
 import os.path as osp
 import numpy as np
+import gym
+import random
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from torchrl.utils import get_args
 from torchrl.utils import get_params
@@ -13,8 +15,6 @@ import torchrl.policies as policies
 import torchrl.networks as networks
 from torchrl.algo import TwinSACQ
 from torchrl.collector.base import VecCollector
-import gym
-import random
 from torchrl.env import get_vec_env
 
 
@@ -63,22 +63,26 @@ def experiment(args):
     params['general_setting']['device'] = device
 
     params['net']['base_type'] = networks.MLPBase
-    params['net']['activation_func'] = torch.nn.Tanh
+    params['net']['activation_func'] = torch.nn.ReLU
 
-    pf = policies.GuassianContPolicy (
-        input_shape = env.observation_space.shape[0], 
-        output_shape = 2 * env.action_space.shape[0],
+    obs_normalizer = env._obs_normalizer if hasattr(env, "_obs_normalizer") \
+        else None
+    params['net']['normalizer'] = obs_normalizer
+
+    pf = policies.GuassianContPolicy(
+        input_shape=env.observation_space.shape[0],
+        output_shape=2 * env.action_space.shape[0],
         **params['net'],
-        **params['policy'] )
-    qf1 = networks.FlattenNet( 
-        input_shape = env.observation_space.shape[0] + env.action_space.shape[0],
-        output_shape = 1,
-        **params['net'] )
+        **params['policy'])
+    qf1 = networks.QNet(
+        input_shape=env.observation_space.shape[0] + env.action_space.shape[0],
+        output_shape=1,
+        **params['net'])
 
-    qf2 = networks.FlattenNet( 
-        input_shape = env.observation_space.shape[0] + env.action_space.shape[0],
-        output_shape = 1,
-        **params['net'] )
+    qf2 = networks.QNet(
+        input_shape=env.observation_space.shape[0] + env.action_space.shape[0],
+        output_shape=1,
+        **params['net'])
 
     print(pf)
     print(qf1)

torchrl/algo/off_policy/twin_sac_q.py
@@ -31,7 +31,9 @@ class TwinSACQ(OffRLAlgo):
         self.qf1 = qf1
         self.qf2 = qf2
         self.target_qf1 = copy.deepcopy(qf1)
+        self.target_qf1.normalizer = self.qf1.normalizer
         self.target_qf2 = copy.deepcopy(qf2)
+        self.target_qf2.normalizer = self.qf2.normalizer
 
         self.to(self.device)
 
@@ -59,7 +61,7 @@ class TwinSACQ(OffRLAlgo):
                 self.target_entropy = target_entropy
             else:
                 self.target_entropy = -np.prod(
-                    self.env.action_space.shape).item()  # from rlkit
+                    self.env.action_space.shape).item()
             self.log_alpha = torch.zeros(1).to(self.device)
             self.log_alpha.requires_grad_()
             self.alpha_optimizer = optimizer_class(
@@ -73,6 +75,13 @@ class TwinSACQ(OffRLAlgo):
         self.policy_mean_reg_weight = policy_mean_reg_weight
 
         self.reparameterization = reparameterization
+
+    # def pretrain(self):
+    #     super().pretrain()
+    #     # All function and share the same normalizer
+    #     # So we only to stop once
+    #     if self.pf.normalizer is not None:
+    #         self.pf.normalizer.stop_update_estimate()
 
     def update(self, batch):
         self.training_update_num += 1
@@ -199,10 +208,10 @@ class TwinSACQ(OffRLAlgo):
         info['log_std/max'] = log_std.max().item()
         info['log_std/min'] = log_std.min().item()
 
-        info['log_probs/mean'] = log_std.mean().item()
-        info['log_probs/std'] = log_std.std().item()
-        info['log_probs/max'] = log_std.max().item()
-        info['log_probs/min'] = log_std.min().item()
+        info['log_probs/mean'] = log_probs.mean().item()
+        info['log_probs/std'] = log_probs.std().item()
+        info['log_probs/max'] = log_probs.max().item()
+        info['log_probs/min'] = log_probs.min().item()
 
         info['mean/mean'] = mean.mean().item()
         info['mean/std'] = mean.std().item()

torchrl/algo/rl_algo.py
@@ -26,6 +26,7 @@ class RLAlgo():
             batch_size=128,
             device='cpu',
             save_interval=100,
+            eval_interval=1,
             save_dir=None):
 
         self.env = env
@@ -60,6 +61,11 @@ class RLAlgo():
         pathlib.Path(self.save_dir).mkdir(parents=True, exist_ok=True)
 
         self.best_eval = None
+        self.eval_interval = eval_interval
+
+        self.explore_time = 0
+        self.train_time = 0
+        self.start = time.time()
 
     def start_epoch(self):
         pass
@@ -97,44 +103,50 @@ class RLAlgo():
             training_epoch_info = self.collector.train_one_epoch()
             for reward in training_epoch_info["train_rewards"]:
                 self.training_episode_rewards.append(reward)
-            explore_time = time.time() - explore_start_time
+
+            self.explore_time += time.time() - explore_start_time
 
             train_start_time = time.time()
             self.update_per_epoch()
-            train_time = time.time() - train_start_time
+            self.train_time += time.time() - train_start_time
 
             finish_epoch_info = self.finish_epoch()
 
-            eval_start_time = time.time()
-            eval_infos = self.collector.eval_one_epoch()
-            eval_time = time.time() - eval_start_time
-
             total_frames += self.epoch_frames
 
-            infos = {}
+            if epoch % self.eval_interval == 0:
+                eval_start_time = time.time()
+                eval_infos = self.collector.eval_one_epoch()
+                eval_time = time.time() - eval_start_time
 
-            for reward in eval_infos["eval_rewards"]:
-                self.episode_rewards.append(reward)
+                infos = {}
 
-            if self.best_eval is None or \
-               (np.mean(eval_infos["eval_rewards"]) > self.best_eval):
-                self.best_eval = np.mean(eval_infos["eval_rewards"])
-                self.snapshot(self.save_dir, 'best')
-            del eval_infos["eval_rewards"]
+                for reward in eval_infos["eval_rewards"]:
+                    self.episode_rewards.append(reward)
 
-            infos["Running_Average_Rewards"] = np.mean(self.episode_rewards)
-            infos["Train_Epoch_Reward"] = \
-                training_epoch_info["train_epoch_reward"]
-            infos["Running_Training_Average_Rewards"] = np.mean(
-                self.training_episode_rewards)
-            infos["Explore_Time"] = explore_time
-            infos["Train___Time"] = train_time
-            infos["Eval____Time"] = eval_time
-            infos.update(eval_infos)
-            infos.update(finish_epoch_info)
+                if self.best_eval is None or \
+                    (np.mean(eval_infos["eval_rewards"]) > self.best_eval):
+                    self.best_eval = np.mean(eval_infos["eval_rewards"])
+                    self.snapshot(self.save_dir, 'best')
+                del eval_infos["eval_rewards"]
 
-            self.logger.add_epoch_info(
-                epoch, total_frames, time.time() - start, infos)
+                infos["Running_Average_Rewards"] = np.mean(
+                    self.episode_rewards)
+                infos["Train_Epoch_Reward"] = \
+                    training_epoch_info["train_epoch_reward"]
+                infos["Running_Training_Average_Rewards"] = np.mean(
+                    self.training_episode_rewards)
+                infos["Explore_Time"] = self.explore_time
+                infos["Train___Time"] = self.train_time
+                infos["Eval____Time"] = eval_time
+                self.explore_time = 0
+                self.train_time = 0
+                infos.update(eval_infos)
+                infos.update(finish_epoch_info)
+
+                self.logger.add_epoch_info(
+                    epoch, total_frames, time.time() - self.start, infos)
+                self.start = time.time()
 
             if epoch % self.save_interval == 0:
                 self.snapshot(self.save_dir, epoch)

torchrl/collector/base.py
@@ -34,7 +34,7 @@ class BaseCollector:
         self.current_ob = self.env.reset()
 
         self.train_rew = 0
-        self.training_episode_rewards = deque(maxlen=20)
+        # self.training_episode_rewards = deque(maxlen=20)
 
         # device specification
         self.device = device
@@ -87,7 +87,7 @@ class BaseCollector:
             # reset current_step
             self.current_step = 0
 
-            self.training_episode_rewards.append(self.train_rew)
+            # self.training_episode_rewards.append(self.train_rew)
             self.train_rews.append(self.train_rew)
             self.train_rew = 0
 
@@ -146,7 +146,7 @@ class BaseCollector:
                     traj_len += 1
                     if self.eval_render:
                         self.eval_env.render()
-                except:
+                except Exception:
                     print(act)
 
             eval_rews.append(rew)
@@ -206,16 +206,15 @@ class VecCollector(BaseCollector):
         }
 
         self.train_rew += reward
+        if np.any(done):
+            self.train_rews.append(list(self.train_rew[done]))
+            self.train_rew[done] = 0
+
         if np.any(done) or \
            np.any(self.current_step >= self.max_episode_frames):
-            next_ob = self.env.partial_reset(np.squeeze(done, axis=-1))
-            self.current_step[done] = 0
-
-            self.training_episode_rewards.append(
-                self.train_rew
-            )
-            self.train_rews.append(self.train_rew[done])
-            self.train_rew[done] = 0
+            flag = (self.current_step >= self.max_episode_frames) | done
+            next_ob = self.env.partial_reset(np.squeeze(flag, axis=-1))
+            self.current_step[flag] = 0
 
         self.replay_buffer.add_sample(sample_dict)
 
@@ -252,7 +251,6 @@ class VecCollector(BaseCollector):
                     exit()
                 try:
                     eval_obs, r, done, _ = self.eval_env.step(act)
-
                     rews = rews + ((1-epi_done) * r)
                     traj_len = traj_len + (1 - epi_done)
 
@@ -264,13 +262,10 @@ class VecCollector(BaseCollector):
 
                     if self.eval_render:
                         self.eval_env.render()
-                except:
+                except Exception:
                     print(act)
-
             eval_rews += list(rews)
             traj_lens += list(traj_len)
-
-            done = False
 
         eval_infos["eval_rewards"] = eval_rews
         eval_infos["eval_traj_length"] = np.mean(traj_lens)

torchrl/env/base_wrapper.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import copy
+import torch
 
 
 class BaseWrapper(gym.Wrapper):
@@ -59,6 +60,46 @@ def update_mean_var_count(
     return new_mean, new_var, new_count
 
 
+class Normalizer():
+    def __init__(self, shape, clip=10.):
+        self.shape = shape
+        self._mean = np.zeros(shape)
+        self._var = np.ones(shape)
+        self._count = 1e-4
+        self.clip = clip
+        self.should_estimate = True
+
+    def stop_update_estimate(self):
+        self.should_estimate = False
+
+    def update_estimate(self, data):
+        if not self.should_estimate:
+            return
+        if len(data.shape) == self.shape:
+            data = data[np.newaxis, :]
+        self._mean, self._var, self._count = update_mean_var_count(
+            self._mean, self._var, self._count,
+            np.mean(data, axis=0), np.var(data, axis=0), data.shape[0])
+
+    def inverse(self, raw):
+        return raw * np.sqrt(self._var) + self._mean
+
+    def inverse_torch(self, raw):
+        return raw * torch.Tensor(np.sqrt(self._var)).to(raw.device) \
+            + torch.Tensor(self._mean).to(raw.device)
+
+    def filt(self, raw):
+        return np.clip(
+            (raw - self._mean) / (np.sqrt(self._var) + 1e-4)
+            -self.clip, self.clip)
+
+    def filt_torch(self, raw):
+        return torch.clamp(
+            (raw - torch.Tensor(self._mean).to(raw.device)) / \
+            (torch.Tensor(np.sqrt(self._var) + 1e-4).to(raw.device)),
+            -self.clip, self.clip)
+
+
 class NormObs(gym.ObservationWrapper, BaseWrapper):
     """
     Normalized Observation => Optional, Use Momentum
@@ -67,41 +108,17 @@ class NormObs(gym.ObservationWrapper, BaseWrapper):
         super(NormObs, self).__init__(env)
         self.count = epsilon
         self.clipob = clipob
-        self._obs_mean = np.zeros(env.observation_space.shape[0])
-        self._obs_var = np.ones(env.observation_space.shape[0])
-
-    def _update_obs_estimate(self, obs):
-        if len(obs.shape) == 1:
-            self._obs_mean, self._obs_var, self.count = update_mean_var_count(
-                self._obs_mean, self._obs_var,
-                self.count, obs, np.zeros_like(obs), 1)
-        elif len(obs.shape) == 2:
-            self._obs_mean, self._obs_var, self.count = update_mean_var_count(
-                self._obs_mean, self._obs_var,
-                self.count, np.mean(obs, axis=0),
-                np.var(obs, axis=0), obs.shape[0])
-        else:
-            raise ValueError("Wrong shape for obs")
+        self._obs_normalizer = Normalizer(env.observation_space.shape)
 
     def copy_state(self, source_env):
-        self._obs_mean = copy.deepcopy(source_env._obs_mean)
+        # self._obs_rms = copy.deepcopy(source_env._obs_rms)
         self._obs_var = copy.deepcopy(source_env._obs_var)
-
-    def _apply_normalize_obs(self, raw_obs):
-        if self.training:
-            self._update_obs_estimate(raw_obs)
-        if len(raw_obs.shape) == 1:
-            return np.clip(
-                (raw_obs - self._obs_mean) / (np.sqrt(self._obs_var) + 1e-8),
-                -self.clipob, self.clipob)
-        elif len(raw_obs.shape) == 2:
-            return np.clip(
-                (raw_obs - self._obs_mean[np.newaxis, ...]) / (
-                    np.sqrt(self._obs_var[np.newaxis, ...]) + 1e-8),
-                -self.clipob, self.clipob)
+        self._obs_mean = copy.deepcopy(source_env._obs_mean)
 
     def observation(self, observation):
-        return self._apply_normalize_obs(observation)
+        if self.training:
+            self._obs_normalizer.update_estimate(observation)
+        return observation
 
 
 class NormRet(BaseWrapper):